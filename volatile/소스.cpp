a23a991939624a0f2648ba14a5cb4ac75e5381a2
revistain <kimrobin150@gmail.com>
2022-11-01 09:39:30 +0900

    레퍼런스 겹침규칙

--- volatile/소스.cpp
+++ volatile/소스.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <vector>
 
+//template <typename T>
+//void wrapper(const T& u) {
+//	g(u);
+//}
+//template <typename T>
+//void wrapper(T& u) {
+//	g(u);
+//}
+
 template <typename T>
-void wrapper(T& u) {
-	g(u);
+void wrapper(T&& u) {
+	// std::forward는 u가 rvalue일때만 move해줌
+	g(std::forward<T>(u));
 }
 
 class A {};
@@ -24,5 +34,16 @@ int main() {
 	std::cout << "wrapper -------------" << std::endl;
 	wrapper(a);
 	wrapper(ca);
-	wrapper(A());
+	wrapper(A()); // const T&로 들어감
+
+	// universial reference
+	// 1.
+	auto&& b = a;	// a는 lvalue이므로 b는 lvalue ref
+
+	// 2.
+	// 템플릿에서의 사용(타입 추론이 필요한 경우에만, const 붙이면 안됨)
+	// &  &  => &
+	// && &  => &
+	// &  && => &
+	// && && => &&
 }
